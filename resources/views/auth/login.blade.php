ac3f3115752bccfefaff9afacc9634ee98cdce72
samsul <samsul.marif100@gmail.com>
2023-07-12 06:45:10 +0800

    ui login ver2 admin lte

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,18 +6,18 @@
 
     <body class="hold-transition login-page">
         <div class="login-box">
-            <div class="login-logo">
-                <a href="{{ url('/') }}"><b>SIPP</b>{{ __('Sistem informasi perencanaan dan penganggaran') }}</a>
-            </div>
             <!-- /.login-logo -->
-            <div class="card">
-                <div class="card-body login-card-body">
+            <div class="card card-outline card-primary">
+                <div class="card-header text-center">
+                    <a href="{{ url('/') }}"
+                        class="h1"><b>SIPP</b>{{ __('Sistem informasi perencanaan dan penganggaran') }}</a>
+                </div>
+                <div class="card-body">
                     <p class="login-box-msg">Sign in to start your session</p>
-
                     <form action="{{ route('login') }}" method="post">
                         @csrf
                         <div class="input-group mb-3">
-                            <input type="email" class="form-control @error('email') is-invalid @enderror" name="email"
+                            <input type="email" class="form-control  @error('email') is-invalid @enderror"
                                 placeholder="Email">
                             <div class="input-group-append">
                                 <div class="input-group-text">
@@ -31,7 +31,7 @@
                             @enderror
                         </div>
                         <div class="input-group mb-3">
-                            <input type="password" class="form-control @error('password') is-invalid @enderror"
+                            <input type="password" class="form-control  @error('password') is-invalid @enderror"
                                 placeholder="Password">
                             <div class="input-group-append">
                                 <div class="input-group-text">
@@ -45,24 +45,32 @@
                             @enderror
                         </div>
                         <div class="row">
+                            <div class="col-8">
+                                <div class="icheck-primary">
+                                    <input type="checkbox" id="remember">
+                                    <label for="remember">
+                                        Remember Me
+                                    </label>
+                                </div>
+                            </div>
                             <!-- /.col -->
                             <div class="col-4">
-                                <button type="submit" class="btn btn-primary btn-block">{{ __('Sign In') }}</button>
+                                <button type="submit" class="btn btn-primary btn-block">Sign In</button>
                             </div>
                             <!-- /.col -->
                         </div>
                     </form>
 
                     <p class="mb-1">
-                        <a href="{{ url('/forgot-password') }}">I forgot my password</a>
+                        <a href="{{ url('forgot-password') }}">Forgot Password</a>
                     </p>
                     <p class="mb-0">
-                        <a href="{{ url('/register') }}" class="text-center">Register a new membership</a>
+                        <a href="{{ route('register') }}" class="text-center">Register</a>
                     </p>
                 </div>
-                <!-- /.login-card-body -->
+                <!-- /.card-body -->
             </div>
+            <!-- /.card -->
         </div>
         <!-- /.login-box -->
-    </body>
-@endsection
+    @endsection
