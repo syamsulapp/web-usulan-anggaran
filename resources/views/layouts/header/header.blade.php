5e71f1f975c9218c656cbd658913ed67ea445ceb
xenrobi <xenrobi@gmail.com>
2023-07-16 10:36:30 +0700

    Update header.blade.php

--- resources/views/layouts/header/header.blade.php
+++ resources/views/layouts/header/header.blade.php
@@ -1,9 +1,9 @@
 <!-- Preloader -->
-{{-- <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="{{ asset('assets/dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo" height="60"
-        width="60">
-</div> --}}
-
+<div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__shake" src="{{ asset('assets/img/logoSIPPWarna.png') }}" alt="logosipp" width="25%"
+        height="25%">
+</div>
+{{-- preloader --}}
 <!-- Navbar -->
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
     <!-- Left navbar links -->
