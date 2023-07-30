c130788270c2ec14a078bab326ff5f108bb157c9
samsul <samsul.marif100@gmail.com>
2023-07-30 18:39:23 +0800

    install module TCPDF and config in app.php

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         \Intervention\Image\ImageServiceProvider::class,
+        \Elibyy\TCPDF\ServiceProvider::class,
     ])->toArray(),
 
     /*
@@ -184,7 +185,8 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
-        'Image' => Intervention\Image\Facades\Image::class
+        'Image' => Intervention\Image\Facades\Image::class,
+        'PDF' => \Elibyy\TCPDF\Facades\TCPDF::class,
     ])->toArray(),
 
 ];
